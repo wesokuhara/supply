8e78b1a992a02ca9d3fc339e129ce1c7a7f0284b
wesokuhara <wesokuhara@gmail.com>
2017-12-02 14:44:33 -0800

    Cleanup bubbleSort

--- src/algorithms/bubbleSort.js
+++ src/algorithms/bubbleSort.js
@@ -10,11 +10,11 @@ const bubbleSort = a => {
   while (swapped) {
     swapped = false;
 
-    for (let j = 0; j < a.length - 1; j++) {
-      if (a[j] > a[j + 1]) {
-        let temp = a[j];
-        a[j] = a[j + 1];
-        a[j + 1] = temp;
+    for (let i = 0; i < a.length - 1; i++) {
+      if (a[i] > a[i + 1]) {
+        let temp = a[i];
+        a[i] = a[i + 1];
+        a[i + 1] = temp;
 
         swapped = true;
       }
