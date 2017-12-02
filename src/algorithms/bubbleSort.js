0fec34dad9b86fd250ddca65e1828795a85a2c76
wesokuhara <wesokuhara@gmail.com>
2017-12-02 14:41:42 -0800

    Update bubbleSort

--- src/algorithms/bubbleSort.js
+++ src/algorithms/bubbleSort.js
@@ -3,22 +3,22 @@ http://www.geeksforgeeks.org/bubble-sort/
 */
 
 const bubbleSort = a => {
-  let swapped;
-  for (let i = 0; i < a.length - 1; i++) {
+  // always check the array once
+  // stop when there are no swaps
+  let swapped = true;
+
+  while (swapped) {
     swapped = false;
 
-    // swap an element with its next element if it is greater
-    // stop when there are no swaps
     for (let j = 0; j < a.length - 1; j++) {
       if (a[j] > a[j + 1]) {
-        swapped = true;
         let temp = a[j];
         a[j] = a[j + 1];
         a[j + 1] = temp;
+
+        swapped = true;
       }
     }
-
-    if (!swapped) break;
   }
 
   return a;
